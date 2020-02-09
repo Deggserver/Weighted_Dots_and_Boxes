720ccec901f1a139546091cf0a6c570ea3ac431b
Deggserver <56365754+Deggserver@users.noreply.github.com>
2020-02-09 05:07:18 -0600

    Initial directory, github, and IDE setup. Added the begginings of the node for states. Will need to reference textbook to ensure I am on the correct path.

--- src/Node.java
+++ src/Node.java
@@ -5,14 +5,22 @@ public class Node {
     private Random random_generator = new Random();
     private boolean[][] dots; //This represents the
     private int[][] boxes; //The boxes that give points
+    private int plies;
 
-    public Node(){
-        this.dots = this.generate_point_array();
+    /**
+     *
+     * @param plies
+     * @param size_board    in terms of boxes
+     */
+    public Node(int plies, int size_board){
+        this.plies = plies;
+        this.dots = this.generate_point_array(size_board);
         this.boxes = this.generate_box_values();
     }
 
-    private boolean[][] generate_point_array(){
-        boolean[][] out = new boolean[5][5];
+    private boolean[][] generate_point_array(int sz){
+        sz += 1;
+        boolean[][] out = new boolean[sz][sz];
         for (int i = 0; i < this.dots.length ; i++) {
             for (int j = 0; j < this.dots[0].length; j++) {
                 out[i][j] = false;
@@ -32,7 +40,7 @@ public class Node {
     }
 
     private int get_next_random_int(){
-        return this.random_generator.nextInt(6); //upper bound
+        return this.random_generator.nextInt(5) + 1; //upper bound
     }
 
 }
